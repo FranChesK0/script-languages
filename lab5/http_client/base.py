19e0b104d32794d84be1e666e3e700c0a4ae0c63
FranChesK0 <khl_doss@mail.ru>
2024-12-16 03:34:02 +0800

    update base http client with enter and exit methods

--- lab5/http_client/base.py
+++ lab5/http_client/base.py
@@ -1,4 +1,5 @@
-import asyncio
+from types import TracebackType
+from typing import Self
 
 from loguru import logger
 from aiohttp import ClientSession
@@ -16,6 +17,16 @@ class HTTPClient:
     def __init__(self, base_url: str, headers: dict[str, str] | None = None) -> None:
         self._session = ClientSession(base_url=base_url, headers=headers)
 
-    def __del__(self) -> None:
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
         logger.info(f"Closing session for {self._session._base_url}.")
-        asyncio.ensure_future(self._session.close())
+        logger.debug(f"Exception: {exc_type}, {exc_value}, {traceback}")
+        if self._session is not None:
+            await self._session.close()
