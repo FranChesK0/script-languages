1f232c0bb2ac52a7051a09073b137bb77ef00af7
FranChesK0 <khl_doss@mail.ru>
2024-12-16 01:29:22 +0800

    create settings

---
+++ lab5/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """
+    Application settings.
+
+    Attributes:
+        ENV (str): The environment to run the application in.
+        BASE_URL (str): The base URL for the application.
+        DATABASE_CONNECTION (str): The database connection string.
+    """
+
+    ENV: str = "dev"
+    BASE_URL: str = "http://jsonplaceholder.typicode.com"
+    DATABASE_CONNECTION: str = "sqlite+aiosqlite:///:memory:"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="allow")
+
+
+settings = Settings()
